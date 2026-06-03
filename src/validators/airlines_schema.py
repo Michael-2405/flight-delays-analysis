61343d478d957a4319fd9984581090f033eaec0a
Michael Espinosa <alexanderbat89@gmail.com>
2026-06-03 19:59:57 -0400

    feat: add airlines schemas with pandera validation

---
+++ src/validators/airlines_schema.py
@@ -0,0 +1,8 @@
+import pandera.polars as pa
+
+airlines_schema = pa.DataFrameSchema(
+    {
+        "IATA_CODE": pa.Column(str, nullable=False),
+        "AIRLINE": pa.Column(str, nullable=False),
+    }
+)
